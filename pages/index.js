9d54ed91d6a4c8d521b1e167d233364d48309ab5
Abhishek Dangol <abhi.dangol@gmail.com>
2021-05-06 23:36:45 -0500

    added backend for form submission

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,31 @@
-import { useRef, useState } from 'react';
+import { useRef } from "react";
+import fs from "fs";
 
 function HomePage() {
-  const [feedbackItems, setFeedbackItems] = useState([]);
-
   const emailInputRef = useRef();
   const feedbackInputRef = useRef();
 
   function submitFormHandler(event) {
     event.preventDefault();
-
-    const enteredEmail = emailInputRef.current.value;
+    const enteredEMail = emailInputRef.current.value;
     const enteredFeedback = feedbackInputRef.current.value;
 
-    const reqBody = { email: enteredEmail, text: enteredFeedback };
-
-    fetch('/api/feedback', {
-      method: 'POST',
-      body: JSON.stringify(reqBody),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+    fetch();
   }
-
-  function loadFeedbackHandler() {
-    fetch('/api/feedback')
-      .then((response) => response.json())
-      .then((data) => {
-        setFeedbackItems(data.feedback);
-      });
-  }
-
   return (
     <div>
       <h1>The Home Page</h1>
       <form onSubmit={submitFormHandler}>
         <div>
-          <label htmlFor='email'>Your Email Address</label>
-          <input type='email' id='email' ref={emailInputRef} />
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef} />
         </div>
         <div>
-          <label htmlFor='feedback'>Your Feedback</label>
-          <textarea id='feedback' rows='5' ref={feedbackInputRef}></textarea>
+          <label htmlFor="feedback">Your Email Address</label>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
         </div>
         <button>Send Feedback</button>
       </form>
-      <hr />
-      <button onClick={loadFeedbackHandler}>Load Feedback</button>
-      <ul>
-        {feedbackItems.map((item) => (
-          <li key={item.id}>{item.text}</li>
-        ))}
-      </ul>
     </div>
   );
 }
